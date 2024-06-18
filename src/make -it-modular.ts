ef226527a7bd3fc3ce1b262d9497f4ce2059984f
Claudio Martin <klaudito46@gmail.com>
2024-06-18 10:32:57 +0200

    make it modular acabado en ts

--- src/make -it-modular.ts
+++ src/make -it-modular.ts
@@ -1,2 +1,12 @@
+const fl = require("./my-module");
 const dirName: string = process.argv[2];
-dirName;
+let userExt: string = "." + process.argv[3];
+
+fl(dirName, userExt, function(err: Error, file: string[]){
+    if (err){
+        console.error(err);
+    }
+    file.forEach((file) => {
+        console.log(file);
+    })
+})
--- src/my-module.ts
+++ src/my-module.ts
@@ -1,5 +1,20 @@
+const path = require("path");
 
+const fs = require("fs");
 
 function listFiles(dirName: string, fileExt: string, callback: CallableFunction) {
-  
+  fs.readdir(dirName, function (err: Error, data: string[]) {
+    let answer: string[];
+    if (err) {
+        return callback(err);
+    }
+    data.forEach(file => {
+        if (path.extname(file) === "." + fileExt) {
+            answer.push(file);
+        }
+        callback(null, answer);
+    });
+    return;
+})
 }
+module.exports = listFiles;
